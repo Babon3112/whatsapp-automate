65e117bff6c6f66838d4373f2a52d3a764f7d74d
Arnab Ghosh <arnabbabon2002@gmail.com>
2024-05-19 16:20:06 +0530

    moved the function to controller

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-import twilio from "twilio";
+import { replyMessage } from "./controller.js";
 
 dotenv.config();
 
@@ -9,41 +9,7 @@ const port = process.env.PORT;
 
 app.use(express.urlencoded({ extended: true }));
 
-const accountSid = process.env.accountSid;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
-
-app.post("/message", async (req, res) => {
-  if (!req.body || !req.body.Body) {
-    return res.status(400).send("Invalid request");
-  }
-
-  console.log(req.body.Body);
-
-  let replyMessage;
-  if (req.body.Body.toLowerCase() === "hi") {
-    replyMessage = "Want to see our catalog?";
-  } else if (req.body.Body.toLowerCase() === "hey") {
-    replyMessage = "How can I help you?";
-  } else {
-    replyMessage = "Welcome to CandidPhotos";
-  }
-
-  const client = twilio(accountSid, authToken);
-
-  await client.messages
-    .create({
-      body: replyMessage,
-      from: "whatsapp:+14155238886",
-      to: `whatsapp:+91${process.env.NUMBER}`,
-    })
-    .then((message) => {
-      res.set("Content-Type", "application/xml");
-      res.send();
-    })
-    .catch((error) => {
-      res.status(500).json({ success: false, msg: error.message });
-    });
-});
+app.post("/message", replyMessage);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
